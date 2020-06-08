54b60e88b133038e5e4f81dee17465bbfa86e43c
wilembergson <wilembergson@yahoo.com>
2020-06-08 14:41:20 -0300

    Saving a new Department

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -7,7 +7,15 @@ import javafx.stage.Stage;
 public class Utils {
 
 	public static Stage currentStage(ActionEvent event) {
-		return (Stage) ((Node) event.getSource()).getScene().getWindow();
-		
+		return (Stage) ((Node) event.getSource()).getScene().getWindow();	
+	}
+	
+	public static Integer tryParseInteger(String str) {
+		try {
+			return Integer.parseInt(str);
+		}
+		catch(NumberFormatException e) {
+			return null;
+		}
 	}
 }
